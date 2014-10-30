0baebba77425a43160d1bcec06de98be8143f9c0
Patrick Stegmann <code@patrick-stegmann.de>
2014-10-30 14:21:46 +0100

    Hooked Doctrine up in ConversionController

--- src/wonderb0lt/CurrencyCalculatorBundle/Controller/ConversionController.php
+++ src/wonderb0lt/CurrencyCalculatorBundle/Controller/ConversionController.php
@@ -2,11 +2,12 @@
 namespace wonderb0lt\CurrencyCalculatorBundle\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class ConversionController {
+class ConversionController extends Controller {
     public function convertAction(Request $request) {
         $sourceCurrency = $request->get("from");
         $destinationCurrency = $request->get("to");
@@ -17,14 +18,27 @@ class ConversionController {
             throw new HttpException(400, "Please pass values for source/destination currency and amount");
         }
 
-        // TODO Mocked response
-        return new JsonResponse(array(
-            "source" => $sourceCurrency,
-            "destination" => $destinationCurrency,
-            "sourceAmount" => $amount,
-            "rate" => 1.27,
-            "value" => $amount * 1.27
-        ));
+        $em = $this->getDoctrine()->getManager();
+        $q = $em->createQuery("SELECT e FROM wonderb0ltCurrencyCalculatorBundle:ExchangeRate e
+                               INNER JOIN e.sourceCurrency src
+                               INNER JOIN e.destinationCurrency dst
+                               WHERE src.isoCode = :src AND dst.isoCode = :dst ");
+
+        $q  ->setParameter("src", $sourceCurrency)
+            ->setParameter("dst", $destinationCurrency);
+        $exchangeRate = $q->getOneOrNullResult();
+
+        if ($exchangeRate !== null) {
+            return new JsonResponse(array(
+                "source" => $sourceCurrency,
+                "destination" => $destinationCurrency,
+                "sourceAmount" => $amount,
+                "rate" => $exchangeRate->getRate(),
+                "value" => $amount * $exchangeRate->getRate()
+            ));
+        } else {
+            throw new HttpException("404", "Conversion unknown");
+        }
     }
 
 } 

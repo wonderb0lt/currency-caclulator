72ae055205e3a5765892bf580c74aa2bdb1d7bb6
Patrick Stegmann <code@patrick-stegmann.de>
2014-10-30 14:58:39 +0100

    Hooked LandingController up to Doctrine proper

--- src/wonderb0lt/CurrencyCalculatorBundle/Controller/LandingController.php
+++ src/wonderb0lt/CurrencyCalculatorBundle/Controller/LandingController.php
@@ -8,6 +8,13 @@ class LandingController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('wonderb0ltCurrencyCalculatorBundle:Landing:index.html.twig', array());
+        $em = $this->getDoctrine()->getManager();
+        $rates =  $em->createQuery("SELECT e FROM wonderb0ltCurrencyCalculatorBundle:ExchangeRate e
+                                    INNER JOIN e.sourceCurrency src
+                                    INNER JOIN e.destinationCurrency dst")
+                     ->getResult();
+        $currencies = $em->createQuery("SELECT c FROM wonderb0ltCurrencyCalculatorBundle:Currency c")
+                         ->getResult();
+        return $this->render('wonderb0ltCurrencyCalculatorBundle:Landing:index.html.twig', array("rates" => $rates, "currencies" => $currencies));
     }
 }

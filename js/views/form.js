e2b680d884bbe072d2f38d263ad00d1fa4455e3f
Patrick Stegmann <code@patrick-stegmann.de>
2014-11-25 16:42:24 +0100

    Proof of concept w/ backbone stack

--- js/views/form.js
+++ js/views/form.js
@@ -15,10 +15,9 @@ define([
                 url: 'https://openexchangerates.org/api/currencies.json?app_id=12a9fc80491043a5a133c3c355dfb874',
                 async: false
             }).responseText);
-            var names = _.values(currencies);
 
             var compiledTemplate = _.template(formTemplate)({
-                currencies: JSON.stringify(names)
+                currencies: currencies
             });
 
             this.$el.append(compiledTemplate);
